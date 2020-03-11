d97c363772f973a929c2a6182125a65fa98585c5
PeteGabriel <pedrogabriel@protonmail.ch>
2020-03-11 13:47:26 +0100

    Add withdraw operation.

--- bank/bank.go
+++ bank/bank.go
@@ -1,7 +1,17 @@
 package bank
 
+import (
+	"fmt"
+)
+
 var deposits = make(chan int)
 var balances = make(chan int)
+var withdraws = make(chan withdraw)
+
+type withdraw struct {
+	amount int
+	notice chan bool
+}
 
 func Deposit(amount int) {
 	deposits <- amount
@@ -15,6 +25,14 @@ func teller() {
 	var balance int //confined to the monitor routine
 	for {
 		select {
+		case withdraw := <- withdraws:
+			t := balance-withdraw.amount
+			if t >= 0 {
+				balance = t
+				withdraw.notice <- true
+			}else {
+				withdraw.notice <- false
+			}
 		case amount := <-deposits:
 			balance += amount
 		case balances <- balance:
@@ -24,4 +42,18 @@ func teller() {
 
 func init() {
 	go teller()
+}
+
+func Withdraw(amount int) bool {
+	notifier := make(chan bool)
+	withdraws <- withdraw{ amount: amount, notice: notifier}
+	for {
+		select {
+		case ok := <-notifier:
+			if !ok {
+				fmt.Println("Insufficient funds")
+			}
+			return ok
+		}
+	}
 }
